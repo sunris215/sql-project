bcaa9455bbd933f93a30947235fac11c65ff9bd8
Mariusz Borkowski <mario@nurtureagency.com>
2017-06-06 16:56:25 +0200

    Added view and first function

--- project1.sql
+++ project1.sql
@@ -32,7 +32,7 @@ IdOsoby VARCHAR(13) NOT NULL REFERENCES Osoby(Pesel), DataZatrudnienia DATE NOT 
 
 CREATE TABLE Przesylki
 (
-IdPrzesylki INT NOT NULL PRIMARY KEY, Rodzaj INT NOT NULL REFERENCES RodzajePaczek(IdRodzaju), Data DATETIME NOT NULL, IdAdresu INT NOT NULL REFERENCES Adresy(IdAdresu), KosztPaczki MONEY
+IdPrzesylki INT NOT NULL PRIMARY KEY, Rodzaj INT NOT NULL REFERENCES RodzajePaczek(IdRodzaju), Data DATETIME NOT NULL, IdAdresu INT NOT NULL REFERENCES Adresy(IdAdresu), KosztPaczki MONEY, Dostarczona BOOLEAN NOT NULL DEFAULT FALSE
 )
 
 CREATE TABLE Zleceniodawcy
@@ -55,7 +55,7 @@ INSERT INTO Pracownicy VALUES (93111712133, '2015-01-01', 3000, 1, 1), (93111212
 
 INSERT INTO RodzajePaczek VALUES (1, 'ZWYKLA', 'STD'), (2, 'ZWYKLA UBEZP', 'STDUB'), (3, 'POBRANIOWA', 'POB'), (4, 'SZYBKA', 'PR1');
 
-INSERT INTO Przesylki VALUES (1, 1, '2017-05-05', 3, 40),(2, 4, '2017-03-05', 4, 30);
+INSERT INTO Przesylki VALUES (1, 1, '2017-05-05', 3, 40),(2, 4, '2017-03-05', 4, 30, FALSE);
 
 INSERT INTO Zleceniodawcy VALUES (91111742133, 1, 'Firma abc'), (83111712133, 2, 'Firma xyz');
 
@@ -64,4 +64,147 @@ INSERT INTO Zleceniodawcy VALUES (91111742133, 1, 'Firma abc'), (83111712133, 2,
 
 SELECT * FROM Przesylki;
 
--- WIDOKI
+------------------------------------------------- Moje widoki
+
+create view WyswietlSamochoduMarkiOpel
+as
+	SELECT DaneSamochodow.Model, DaneSamochodow.Pojemność, DaneSamochodow.IloscMiejsc
+	FROM DaneSamochodow
+	WHERE Marka='Opel'
+
+select * from WyswietlSamochoduMarkiOpel
+----------------------------------------------------------------------------------
+
+create view WyswietlPaczki
+as
+	SELECT RodzajePaczek.Nazwa, RodzajePaczek.Nazwa
+	FROM RodzajePaczek
+
+select * from WyswietlPaczki
+----------------------------------------------------------------------------------
+
+create view WyswietlSzczegoloweDanePracownikow
+as
+	SELECT O.Imie, O.Nazwisko, O.Pesel, P.DataZatrudnienia, P.Pensja
+	FROM Osoby O JOIN Pracownicy P
+	On P.IdOsoby = O.Pesel
+
+select * from WyswietlSzczegoloweDanePracownikow
+----------------------------------------------------------------------------------
+
+create view WyswietlZarobkiNaDanyZespol
+as
+	SELECT IdSamochodu, IdZespolu, (IloscDowiezionychPaczek * Prowizja) AS Zarobki
+	FROM PrzydzialSamochodow
+
+select * from WyswietlZarobkiNaDanyZespol
+----------------------------------------------------------------------------------
+
+create view WyswietlTegorocznePaczki
+as
+	SELECT P.IdPrzesylki, P.Dostarczona, R.Nazwa, R.Oznaczenie, P.Data
+	FROM Przesylki P JOIN RodzajePaczek R
+	On P.Rodzaj=R.IdRodzaju
+	WHERE DATEPART(YEAR,P.Data) = DATEPART(YEAR,GETDATE())
+
+select * from WyswietlTegorocznePaczki
+----------------------------------------------------------------------------------
+
+create view WyswietlZleceniodawcowOrazIchPrzesylki
+as
+	SELECT Z.IdOsoby, O.Imie, O.Nazwisko, A.Miejscowosc, A.NrDomu, P.IdPrzesylki, P.Data
+	FROM Zleceniodawcy Z
+
+	LEFT JOIN Osoby O
+	ON Z.IdOsoby = O.Pesel
+	JOIN Przesylki P
+	ON Zleceniodawcy.IdZgloszenia = P.IdPrzesylki
+	JOIN Adresy A
+	ON A.IdAdresu = O.IdAdresu
+
+select * from WyswietlZleceniodawcowOrazIchPrzesylki
+----------------------------------------------------------------------------------
+
+create view WyswietlDochody
+as
+	SELECT Z.
+	FROM Zleceniodawcy Z RIGHT JOIN Przesylki P
+	ON Z.IdZgloszenia=P.IdPrzesylki
+	LEFT JOIN Osoby O
+	ON Z.IdOsoby = O.Pesel
+
+select * from WyswietlDochody
+----------------------------------------------------------------------------------
+
+create view WyswietlRoczajePaczek
+as
+	SELECT Nazwa, Oznaczenie, Przesylki.
+	FROM RodzajePaczek
+
+select * from WyswietlRoczajePaczek
+----------------------------------------------------------------------------------
+------------------------------------------------- Procedury
+
+
+
+
+
+
+
+-- ------------------------------------- Aktualizuje przewodnika dla podanej grupy
+-- CREATE PROCEDURE Pr2
+-- (@NazwaGrupy nvarchar(30), @PrzewodnikID int)
+-- AS
+-- 	UPDATE Grupy  SET Grupy.PrzewodnikID=@PrzewodnikID
+-- 	WHERE Grupy.NazwaGrupy=@NazwaGrupy
+--
+-- exec Pr2 'Pracownicy PKP', 2
+--
+-- ------------------------------------- Wyswietla dane wycieczki podanej jako argument
+-- Create PROCEDURE Pr3
+-- @Miejsce_wycieczki nvarchar(30)
+-- AS
+-- 	SELECT * FROM Wycieczki
+-- 	WHERE Wycieczki.Cel=@Miejsce_wycieczki
+--
+-- exec Pr3 'Dubrownik'
+--
+-- ------------------------------------- Dodaje nowego przewodnika
+-- Create PROCEDURE Pr4
+-- @PracownikID int, @Imie nvarchar(20), @Nazwisko nvarchar(30), @status nvarchar(3)
+-- as
+-- 	INSERT INTO Przewodnicy values (@PracownikID,@Imie,@Nazwisko,@status)
+--
+-- exec Pr4 8,'Lucjan','Nowak','Tak'
+
+
+
+------------------------------------- wypisuje paczki o cennie wiekszej niz podana good
+CREATE PROCEDURE Pr8
+(@Koszt float)
+AS
+	SELECT * FROM Przesylki
+	WHERE Przesylki.KosztPaczki>@Koszt
+
+exec  Pr8 500
+
+
+
+-- ------------------------------------------------- Funkcje
+--
+-- ------------------------------------- wypisuje ilosc wolnych miejsc w podanej wycieczce
+-- create function fun1
+-- (@IdWycieczki int)
+-- returns table
+-- AS
+-- return
+-- (
+-- 	select W.IloscMiejsc, G.IloscOsob as IloscWolnychMiejsc
+-- 	from Wycieczki W join Terminy T
+-- 	on W.WycieczkaID=T.WycieczkaID
+-- 	join Grupy G
+-- 	on G.GrupaID=T.GrupaID
+-- 	where T.WycieczkaID=@IdWycieczki
+-- )
+--
+-- select * from fun1 (1)
