0df9319d57a4624f77468899239fca893c932ead
Mariusz Borkowski <mario@nurtureagency.com>
2017-06-09 14:31:22 +0200

    Added views

--- project1.sql
+++ project1.sql
@@ -3,7 +3,8 @@ CREATE DATABASE Projekt_1
 Use Projekt_1
 CREATE TABLE DaneSamochodow
 (
-IdSamochodu INT NOT NULL PRIMARY KEY, Marka VARCHAR(30) NOT NULL,Model VARCHAR(30) NOT NULL, Pojemność INT, IloscMiejsc INT, NrRejestracyjny VARCHAR(8) NOT NULL, RokProdukcji DATETIME
+IdSamochodu INT NOT NULL PRIMARY KEY, Marka VARCHAR(30) NOT NULL,Model VARCHAR(30) NOT NULL, Pojemność INT,
+	IloscMiejsc INT, NrRejestracyjny VARCHAR(8) NOT NULL, RokProdukcji DATETIME
 )
 
 CREATE TABLE RodzajePaczek
@@ -13,61 +14,73 @@ IdRodzaju INT NOT NULL PRIMARY KEY, Nazwa VARCHAR(30) NOT NULL, Oznaczenie VARCH
 
 CREATE TABLE PrzydzialSamochodow
 (
-IdZespolu INT NOT NULL PRIMARY KEY, NazwaObszaru VARCHAR(60) NOT NULL, IloscDowiezionychPaczek INT NOT NULL, Prowizja MONEY NOT NULL, IdSamochodu INT REFERENCES DaneSamochodow(IdSamochodu)
+IdZespolu INT NOT NULL PRIMARY KEY, NazwaObszaru VARCHAR(60) NOT NULL, IloscDowiezionychPaczek INT NOT NULL,
+	Prowizja MONEY NOT NULL, IdSamochodu INT REFERENCES DaneSamochodow(IdSamochodu)
 )
 
 CREATE TABLE Adresy
 (
-IdAdresu INT NOT NULL PRIMARY KEY, Miejscowosc VARCHAR(30) NOT NULL, Ulica VARCHAR(30) NOT NULL, NrDomu SMALLINT NOT NULL,
+IdAdresu INT NOT NULL PRIMARY KEY, Miejscowosc VARCHAR(30) NOT NULL, Ulica VARCHAR(30) NOT NULL,
+	NrDomu SMALLINT NOT NULL,
 )
 
 CREATE TABLE Osoby
 (
-Pesel VARCHAR(13) NOT NULL PRIMARY KEY, Imie VARCHAR(30) NOT NULL, Nazwisko VARCHAR(30) NOT NULL,  IdAdresu INT NOT NULL REFERENCES Adresy(IdAdresu)
+Pesel VARCHAR(13) NOT NULL PRIMARY KEY, Imie VARCHAR(30) NOT NULL, Nazwisko VARCHAR(30) NOT NULL,
+	IdAdresu INT NOT NULL REFERENCES Adresy(IdAdresu)
 )
 
 CREATE TABLE Pracownicy
 (
-IdOsoby VARCHAR(13) NOT NULL REFERENCES Osoby(Pesel), DataZatrudnienia DATE NOT NULL, Pensja MONEY NOT NULL, PrawoJazdy BIT NOT NULL, IdZespolu INT NOT NULL REFERENCES PrzydzialSamochodow(IdZespolu)
+IdOsoby VARCHAR(13) NOT NULL REFERENCES Osoby(Pesel), DataZatrudnienia DATE NOT NULL, Pensja MONEY NOT NULL,
+	PrawoJazdy BIT NOT NULL, IdZespolu INT NOT NULL REFERENCES PrzydzialSamochodow(IdZespolu)
 )
 
 CREATE TABLE Przesylki
 (
 IdPrzesylki INT NOT NULL PRIMARY KEY, Rodzaj INT NOT NULL REFERENCES RodzajePaczek(IdRodzaju), 
-Data DATETIME NOT NULL, IdAdresu INT NOT NULL REFERENCES Adresy(IdAdresu), KosztPaczki MONEY, Dostarczona BIT NOT NULL DEFAULT (0)
+Data DATETIME NOT NULL, IdAdresu INT NOT NULL REFERENCES Adresy(IdAdresu), KosztPaczki MONEY,
+	Dostarczona BIT NOT NULL DEFAULT (0)
 )
 
 CREATE TABLE Zleceniodawcy
 (
-IdOsoby VARCHAR(13) NOT NULL REFERENCES Osoby(Pesel), IdZgloszenia INT NOT NULL REFERENCES Przesylki(IdPrzesylki), DodatkoweInformacje VARCHAR(400) NOT NULL,
+IdOsoby VARCHAR(13) NOT NULL REFERENCES Osoby(Pesel), IdZgloszenia INT NOT NULL REFERENCES Przesylki(IdPrzesylki),
+	DodatkoweInformacje VARCHAR(400) NOT NULL,
 )
 
+----------------------------------------------------------------------------------
 
 -- Dodawanie danych
 
-INSERT INTO Adresy VALUES (1, 'Bobowa', 'Grunwaldzka', 84), (2, 'Nowy Sacz', '3 Maja', 12), (3, 'Gorlice', 'Lwowska', 5), (4, 'Kraków', 'Rynek', 10);
+INSERT INTO Adresy VALUES (1, 'Bobowa', 'Grunwaldzka', 84), (2, 'Nowy Sacz', '3 Maja', 12),
+	(3, 'Gorlice', 'Lwowska', 5), (4, 'Kraków', 'Rynek', 10);
 
-INSERT INTO Osoby VALUES (93111712133, 'Jan', 'Kowalski', 1), (93111212133, 'Maciek', 'Borkowski', 2), (91111742133, 'Adam', 'Poniatowski', 3), (83111712133, 'Mariola', 'Katra', 4);
+INSERT INTO Osoby VALUES (93111712133, 'Jan', 'Kowalski', 1), (93111212133, 'Maciek', 'Borkowski', 2),
+	(91111742133, 'Adam', 'Poniatowski', 3), (83111712133, 'Mariola', 'Katra', 4);
 
-INSERT INTO DaneSamochodow VALUES (1, 'Fiat', 'Ducato', 3000, 3, 'KGR44AA', '2008-01-01'), (2, 'Iveco', 'Daily', 3500, 2, 'KR G4DS', '2010-01-01');
+INSERT INTO DaneSamochodow VALUES (1, 'Fiat', 'Ducato', 3000, 3, 'KGR44AA', '2008-01-01'),
+	(2, 'Iveco', 'Daily', 3500, 2, 'KR G4DS', '2010-01-01');
 
 INSERT INTO PrzydzialSamochodow VALUES (1, 'KGR', 1234, 200, 1), (2, 'KRA', 200, 200, 2),(3, 'KNS', 100, 100, NULL) ;
 
 INSERT INTO Pracownicy VALUES (93111712133, '2015-01-01', 3000, 1, 1), (93111212133, '2013-01-01', 4000, 1, 2);
 
-INSERT INTO RodzajePaczek VALUES (1, 'ZWYKLA', 'STD'), (2, 'ZWYKLA UBEZP', 'STDUB'), (3, 'POBRANIOWA', 'POB'), (4, 'SZYBKA', 'PR1');
+INSERT INTO RodzajePaczek VALUES (1, 'ZWYKLA', 'STD'), (2, 'ZWYKLA UBEZP', 'STDUB'), (3, 'POBRANIOWA', 'POB'),
+	(4, 'SZYBKA', 'PR1');
 
 INSERT INTO Przesylki VALUES (1, 1, '2017-05-05', 3, 40, 2),(2, 4, '2017-03-05', 4, 30, 0);
 
 INSERT INTO Zleceniodawcy VALUES (91111742133, 1, 'Firma abc'), (83111712133, 2, 'Firma xyz');
 
+----------------------------------------------------------------------------------
 
--- ODCZYT DANYCH
+------------------------------------- ODCZYT DANYCH ------------------------------
 
 SELECT * FROM Przesylki;
 SELECT * FROM DaneSamochodow;
 
-------------------------------------------------- Moje widoki
+------------------------------------- Moje widoki --------------------------------
 
 create view WyswietlSamochoduMarkiFiat
 as
@@ -76,6 +89,7 @@ as
 	WHERE Marka='Fiat'
 
 select * from WyswietlSamochoduMarkiFiat
+
 ----------------------------------------------------------------------------------
 
 create view WyswietlPaczki
@@ -84,6 +98,7 @@ as
 	FROM RodzajePaczek
 
 select * from WyswietlPaczki
+
 ----------------------------------------------------------------------------------
 
 create view WyswietlSzczegoloweDanePracownikow
@@ -93,6 +108,7 @@ as
 	On P.IdOsoby = O.Pesel
 
 select * from WyswietlSzczegoloweDanePracownikow
+
 ----------------------------------------------------------------------------------
 
 create view WyswietlZarobkiNaDanyZespol
@@ -101,6 +117,7 @@ as
 	FROM PrzydzialSamochodow
 
 select * from WyswietlZarobkiNaDanyZespol
+
 ----------------------------------------------------------------------------------
 
 create view WyswietlTegorocznePaczki
@@ -111,6 +128,7 @@ as
 	WHERE DATEPART(YEAR,P.Data) = DATEPART(YEAR,GETDATE())
 
 select * from WyswietlTegorocznePaczki
+
 ----------------------------------------------------------------------------------
 
 create view WyswietlZleceniodawcowOrazIchPrzesylki
@@ -126,6 +144,7 @@ as
 	ON A.IdAdresu = O.IdAdresu
 
 select * from WyswietlZleceniodawcowOrazIchPrzesylki
+
 ----------------------------------------------------------------------------------
 
 create view WyswietlDochody
@@ -137,6 +156,7 @@ as
 	ON Z.IdOsoby = O.Pesel
 
 select * from WyswietlDochody
+
 ----------------------------------------------------------------------------------
 
 create view WyswietlRoczajePaczek
@@ -145,13 +165,15 @@ as
 	FROM RodzajePaczek
 
 select * from WyswietlRoczajePaczek
+
 ----------------------------------------------------------------------------------
 
 
-------------------------------------------------- Procedury
+------------------------------------- PROCEDURY ----------------------------------
 
 
-------------------------------------- Aktualizuje adres
+------------------------------------- Aktualizuje adres --------------------------
+
 CREATE PROCEDURE Pr1
 (@IdAdresu INT, @Miejscowosc nvarchar(30),  @Ulica nvarchar(30), @NrDomu INT)
 AS
@@ -162,7 +184,8 @@ exec Pr1 1,'Nowy Sacz', 'Lwowska', 44
 
 SELECT * FROM Adresy WHERE IdAdresu = 1;
 
---------------------------------------- Wyswietla samochod o pojemnosci podanej jako argument
+------------------ Wyswietla samochod o pojemnosci podanej jako argument -------
+
 Create PROCEDURE Pr6
 @Pojemnosc INT
 AS
@@ -171,7 +194,8 @@ AS
 
 exec Pr6 '3500'
 
-------------------------------------- Dodaje nowego zleceniodawce
+----------------- Dodaje nowego zleceniodawce -----------------------------------
+
 Create PROCEDURE Pr7
 @IdOsoby INT, @IdZgloszenia INT, @IdPrzesylki INT, @DodatkoweInformacje nvarchar(400)
 as
@@ -179,7 +203,8 @@ as
 
 exec Pr7 93111712133, 92333, 'Firma Nazwa'
 
-------------------------------------- wypisuje paczki o cennie wiekszej niz podana good
+----------------- Wypisuje paczki o cennie wiekszej niz podana good ------------
+
 CREATE PROCEDURE Pr8
 (@Koszt float)
 AS
@@ -188,34 +213,28 @@ AS
 
 exec  Pr8 5
 
--- ------------------------------------------------- Funkcje
+------------------------------------- FUNCKJE ---------------------------------
 
--- ------------------------------------- wypisuje imie i nazwisko o danym id todo zrobic funkcje
--- create function fun1
--- (@IdOsoby int)
--- returns table
--- AS
--- return
--- (
--- 	select W.IloscMiejsc, G.IloscOsob as IloscWolnychMiejsc
--- 	from Wycieczki W join Terminy T
--- 	on W.WycieczkaID=T.WycieczkaID
--- 	join Grupy G
--- 	on G.GrupaID=T.GrupaID
--- 	where T.WycieczkaID=@IdWycieczki
--- )
---
--- select * from fun1 (1)
+---------------- Wypisuje imie i nazwisko osovt danym id pracownika -----------
 
---todo
---todo zrobic opis, sprawdzic czy dziala wszsystko, sprawdzic czy wszystko z listy jest zrobione
+create function fun1 -- todo Dawid can you chceck it?
+(@IdOsoby int)
+returns table
+AS
+return
+(
+	select O.Imie as Imie, O.Nazwisko as Nazwisko
+	from Pracownicy P join Osoby O
+	on P.IdOsoby=O.Pesel
+	where P.IdOsoby=@IdOsoby
+)
 
+select * from fun1 (1)
 
+------------------------------------- TRIGGERY ---------------------------------
 
-
--- ------ TRIGGERY 
-
--- Opis: trigger zabezpiecza bazę przed cofaniem "licznika" dowiezionych paczek w tabeli "PrzydzialSamochodow" 
+-- Opis: trigger zabezpiecza bazę przed cofaniem "licznika" dowiezionych paczek
+	-- w tabeli "PrzydzialSamochodow"
 
 CREATE TRIGGER T1 ON PrzydzialSamochodow AFTER UPDATE
 AS
@@ -237,7 +256,8 @@ UPDATE PrzydzialSamochodow SET IloscDowiezionychPaczek = 2445 WHERE IdZespolu = 
 
 
 -- Opis: trigger zabezpiecza przed aktualizacja wieku samochodow 
--- Wraz z triggerem został stworzony Check Constraint dla Insertów - wymagany jest wiek młodszy niż 17 lat 
+-- Wraz z triggerem został stworzony Check Constraint dla Insertów - wymagany
+	-- jest wiek młodszy niż 17 lat
  
 CREATE TRIGGER T2 ON DaneSamochodow AFTER UPDATE
 AS
@@ -280,3 +300,7 @@ END
 INSERT INTO Osoby VALUES ('111111111112', 'Andrzej', 'Nowacki', 1);
 
 SELECT * FROM Osoby
+
+
+--todo
+--todo zrobic opis, sprawdzic czy dziala wszsystko, sprawdzic czy wszystko z listy jest zrobione
